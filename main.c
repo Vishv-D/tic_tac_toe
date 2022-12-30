dd12f2b457616f426e0d3e134de8797e73258a48
Vishv <113276106+Vishv-D@users.noreply.github.com>
2022-12-30 19:39:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,217 +1,120 @@
-/*# tic_tac_toe
-This is a simple tic tac toe game which has is created in c and in in this game player can play this game and also save the score for future reviewing in a text file*/
-
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <conio.h>
 
-char x,o;
-char a[9]={'1','2','3','4','5','6','7','8','9'};
-char u1[50],u2[50];
-void board();
-void rules();
-int checkforwin();
-int decision();
-int main()
-{
-    FILE *p;
-    p=fopen("score.txt","a+");
-    fclose(p);
-    system("color 09");
-    int player=1;
-    int choice,score=-1;
-    char symbol,re;
-    char start,dec;
-    int s;
-    rules();
-    printf("\n\nType 1 to start the game:-\nType 2 to view leader board:-\n");
-    scanf("%d",&s);
-    if(s==1)
-    {
-    read:
-        p=fopen("score.txt","a+");
-    printf("\nEnter name of player1: ");
-    scanf("%s",u1);
-    fprintf(p,"\n%s",u1);
-    printf("Enter name of player2: ");
-    scanf("%s",u2);
-    fprintf(p,"\t%s",u2);
-    fclose(p);
-    if(!strcmp(u1,u2))
-    {
-        printf("Enter names of different players!\n\n");
-        goto read;
-    }
-    else
-        decision();
+#define ROWS 3
+#define COLS 3
+#define MAX_NAME_LEN 50
 
+typedef struct {
+char name[MAX_NAME_LEN];
+char symbol;
+} Player;
 
-    system("color fc");
-    board();
+char board[ROWS][COLS];
+Player players[2];
 
-    do
-    {
-
-        player=((player%2)?1:2);
-        if(player==1)
-        printf("%s Type any digit from 1-9 to fill your response:- ",u1);
-        else
-            printf("%s Type any digit from 1-9 to fill your response:- ",u2);
-        scanf("%d",&choice);
-        symbol=((player==1)?x:o);
-        if(choice==1 && a[0]=='1')
-            a[0]=symbol;
-        else if(choice==2 && a[1]=='2')
-            a[1]=symbol;
-        else if(choice==3 && a[2]=='3')
-            a[2]=symbol;
-        else if(choice==4 && a[3]=='4')
-            a[3]=symbol;
-        else if(choice==5 && a[4]=='5')
-            a[4]=symbol;
-        else if(choice==6 && a[5]=='6')
-            a[5]=symbol;
-        else if(choice==7 && a[6]=='7')
-            a[6]=symbol;
-        else if(choice==8 && a[7]=='8')
-            a[7]=symbol;
-        else if(choice==9 && a[8]=='9')
-            a[8]=symbol;
-        else
-            {printf("Wrong Selection\n");player--;}
-
-        score=checkforwin();
-        player++;
-        board();
-    }while(score == -1);
-
-
-    p=fopen("score.txt","a+");
-    if(score==1)
-    {
-
-        if(player==2)
-        {printf("\n\nPlayer1 %s Wins!\n\n",u1);fprintf(p,"\t%s",u1);
-        getch();}
-        else
-            {printf("\n\nPlayer2 %s Wins!\n\n",u2);fprintf(p,"\t%s",u2);
-        getch();
-            }
-        fclose(p);
-    }
-    else
-        printf("\n\nGame Draws!\n\n");fprintf(p,"\t%s","DRAW");
-        getch();
-    }
-    if(s==2)
-    {
-        int cho;
-        system("cls");
-        printf("\n\n");
-        printf("\tLEADERBOARD\n\n");
-        char c;
-        p=fopen("score.txt","r");
-        while((c=getc(p))!=EOF)
-        {
-            printf("%c",c);
-        }
-        fclose(p);
-        printf("\n\nPress 1 to start the game:- ");
-        scanf("%d",&cho);
-        if(cho==1)
-            goto read;
-        else
-            getch();
-    }
-    else
-    {
-        printf("\n\nShould have typed 1 to play the game!\nHope to see you back soon!\n\n");
-        getch();
-    }
-}
-int checkforwin()
-{
-    if(a[0]==a[1] && a[1]==a[2])
-        return 1;
-    else if(a[3]==a[4] && a[4]==a[5])
-        return 1;
-    else if(a[6]==a[7] && a[7]==a[8])
-        return 1;
-    else if(a[0]==a[3] && a[3]==a[6])
-        return 1;
-    else if(a[1]==a[4] && a[4]==a[7])
-        return 1;
-    else if(a[2]==a[5] && a[5]==a[8])
-        return 1;
-    else if(a[0]==a[4] && a[4]==a[8])
-        return 1;
-    else if(a[2]==a[4] && a[4]==a[6])
-        return 1;
-    else if(a[0]!='1' && a[1]!='2' && a[2]!='3' && a[3]!='4' && a[4]!='5' && a[5]!='6' && a[6]!='7' && a[7]!='8' && a[8]!='9')
-        return 0;
-    else
-        return -1;
+void print_board() {
+printf("\n %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+printf(" -----------\n");
+printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+printf(" -----------\n");
+printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
 }
 
-void board()
-{
-    int i;
-
-    system("cls");
-    printf("\tTic-Tac-Toe\n\n");
-        printf("\n\n");
-        printf("%s:- (%c)\n%s:-  (%c)\n\n\n",u1,x,u2,o);
-
-        printf("  %c |  %c | %c\n",a[0],a[1],a[2]);
-        printf("    |    |    \n");
-        printf("----|----|----\n");
-        printf("    |    |    \n");
-        printf("  %c |  %c | %c\n",a[3],a[4],a[5]);
-        printf("    |    |    \n");
-        printf("----|----|----\n");
-        printf("  %c |  %c | %c\n",a[6],a[7],a[8]);
-        printf("    |    |    \n");
-    }
-void rules()
-{
-    char link;
-    printf("\tTic-Tac-Toe\n\n");
-    printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
-    printf("Rules:-\n");
-    printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
-    printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
-    printf("\n\nEnjoy the game! Be a Winner!\n\n");
-    printf("For more clarifications press Y else type any other character:- ");
-    scanf("%c",&link);
-    if(link=='y' || link=='Y')
-    {
-        system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
-    }
-
+int check_win() {
+// check rows
+for (int i = 0; i < ROWS; i++) {
+if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+return 1;
 }
-int decision()
-{
-    char dec;
-        deci:
-        printf("\n\nPlayer1 %s choose the X or 0:",u1);
-        dec=getchar();
-        scanf("%c",&dec);
-        {
-            if(dec=='X' || dec=='x')
-            {
-                x='X';
-                o='0';
-            }
-            else if(dec=='0')
-            {
-                x='0';
-                o='X';
-            }
-            else
-            {
-                printf("Please enter either X or 0 only \n\n");
-                goto deci;
-            }
-        }
 }
+
+// check columns
+for (int i = 0; i < COLS; i++) {
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+        return 1;
+    }
+}
+
+// check diagonals
+if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    return 1;
+}
+if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    return 1;
+}
+
+return 0;
+}
+
+int check_draw() {
+for (int i = 0; i < ROWS; i++) {
+for (int j = 0; j < COLS; j++) {
+if (board[i][j] == ' ') {
+return 0;
+}
+}
+}
+return 1;
+}
+
+void reset_board() {
+for (int i = 0; i < ROWS; i++) {
+for (int j = 0; j < COLS; j++) {
+board[i][j] = ' ';
+}
+}
+}
+
+int main() {
+printf("Welcome to Tic-Tac-Toe!\n");
+printf("Enter the names of the players: ");
+scanf("%s %s", players[0].name, players[1].name);
+
+
+if (strcmp(players[0].name, players[1].name) == 0) {
+    printf("Error: Players must have different names.\n");
+    return 1;
+}
+
+reset_board();
+players[0].symbol = 'X';
+players[1].symbol = 'O';
+
+int player = 0;
+int row, col;
+while (1) {
+    print_board();
+    printf("%s's turn. Enter row and column (1-3): ", players[player].name);
+    scanf("%d %d", &row, &col);
+
+    if (row < 1 || row > 3 || col < 1 || col > 3) {
+        printf("Invalid move.\n");
+        continue;
+    }
+
+    row--;
+    col--;
+    if (board[row][col] != ' ') {
+        printf("That space is already taken.\n");
+        continue;
+    }
+
+    board[row][col] = players[player].symbol;
+    if (check_win()) {
+        print_board();
+        printf("%s wins!\n", players[player].name);
+        break;
+    }
+    if (check_draw()) {
+        print_board();
+        printf("It's a draw!\n");
+        break;
+    }
+    player = (player + 1) % 2;
+}
+
+return 0;
+}
+
